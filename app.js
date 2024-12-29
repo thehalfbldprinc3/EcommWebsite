bb1d91e2c63611636f8fbb0a5dd008a6ae5d32b9
TheHalfBldPrinc3 <dhruvkhatri1234@gmail.com>
2024-12-30 01:55:42 +0530

    Last commit on mon Dec30th 2024 working on delete button of the cart

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 const PORT= 8080;
 const express = require('express');
 const app = express();
--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const {isLoggedIn} = require('../middleware');
-const Product = require('../models/product');
+const Product = require('../models/Product');
 const User = require("../models/User");
 //Route to see the cart
 router.get('/user/cart', isLoggedIn, async (req, res) => {
     let user= await User.findById(req.user._id).populate('cart');
-    res.render('cart/cart.ejs',{user});
+    const totalAmount=user.cart.reduce((sum,curr) => sum + curr.price,0);
+    const productInfo = user.cart.map((p)=>p.descr).join(',');
+    res.render('cart/cart.ejs',{user,totalAmount,productInfo});
 })
 
 router.post('/user/:productID/add', async (req, res) => {
@@ -18,5 +20,6 @@ router.post('/user/:productID/add', async (req, res) => {
     await user.save();
     res.redirect('/user/cart');
 })
-
+//delete function
+router.delete('user/:productID/delete', async (req, res) => {})
 module.exports = router;
